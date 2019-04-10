efd8fad556143c642fb486d1f87bf4970599c488
maksym.ignatchenko <maksym.ignatchenko@appus.me>
2019-04-10 18:07:49 +0300

    modify proof detail page

--- app/Modules/Challenges/Resources/views/proof/datatables_actions.blade.php
+++ app/Modules/Challenges/Resources/views/proof/datatables_actions.blade.php
@@ -2,7 +2,9 @@
     <a href="{{ route('challenge.proof.show', ['challenge_id' => $challenge_id, 'proof' => $id]) }}" class='btn btn-primary'>
         <i class="glyphicon glyphicon-eye-open"></i>
     </a>
-    @if($status === \App\Modules\Challenges\Enums\ProofStatusEnum::PENDING)
+</div>
+@if($status === \App\Modules\Challenges\Enums\ProofStatusEnum::PENDING)
+<div class='btn-group'>
     <a href="#" class='btn btn-success'
         onclick="document.getElementById('accept-{{$id}}-button').click()">
         {!! Form::open(['method'=>'PUT', 'url'=> route('challenge.proof.update', ['challenge' => $challenge_id, 'proof' => $id])]) !!}
@@ -14,6 +16,8 @@
         {!! Form::close() !!}
         <i class="glyphicon glyphicon-ok"></i>
     </a>
+</div>
+<div class='btn-group'>
     <a href="#" class='btn btn-danger'
         onclick="document.getElementById('reject-{{$id}}-button').click()">
         {!! Form::open(['method'=>'PUT', 'url'=> route('challenge.proof.update', ['challenge' => $challenge_id, 'proof' => $id])]) !!}
@@ -25,5 +29,6 @@
         {!! Form::close() !!}
         <i class="glyphicon glyphicon-remove"></i>
     </a>
-    @endif
 </div>
+@endif
+
--- app/Modules/Challenges/Resources/views/proof/show_fields.blade.php
+++ app/Modules/Challenges/Resources/views/proof/show_fields.blade.php
@@ -2,72 +2,37 @@
     <div class="col-md-6">
         <div class="box">
             <div class="box-body">
-                <!-- Name Field -->
-                <div class="form-group">
-                    <p>
-                        {{ Form::label('user', 'User ') }}
-                        {!!  Html::link($dto->getUserLink(), $dto->getUserName()) !!}
-                    </p>
+                <div class="card m-b-20 card-inverse text-black" style="background-color: #f0f1f4; border-color: white;">
+                    <div class="card-block">
+                        <div class="row">
+                            <div class="col-md-4"><p class="line-no-break">USER</p></div>
+                            <div class="col-md-8"><p class="line-no-break">{!!  Html::link($dto->getUserLink(), $dto->getUserName()) !!}</p></div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-4"><p class="line-no-break">CHALLENGE</p></div>
+                            <div class="col-md-8"><p class="line-no-break">{!!  Html::link($dto->getChallengeLink(), $dto->getChallengeName()) !!}</p></div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-md-6">
-        <div class="box">
-            <div class="box-body">
-                <!-- Name Field -->
-                <div class="form-group">
-                    <p>
-                        {{ Form::label('challenge', 'Challenge ') }}
-                        {!!  Html::link($dto->getChallengeLink(), $dto->getChallengeName()) !!}
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 
-<div class="row form-justify-container">
-    <div class="col-md-6">
-        <div class="box">
-            <div class="box-body">
-                <!-- Status Field -->
-                <div class="form-group">
-                    <p>
-                        {{ Form::label('status', 'Status ') }}
-                        {{ $dto->getProofStatus() }}
-                    </p>
-                </div>
             </div>
         </div>
     </div>
     <div class="col-md-6">
         <div class="box">
             <div class="box-body">
-                <!-- Send time Field -->
-                <div class="form-group">
-                    <p>
-                        {{ Form::label('created_at', 'Send time ') }}
-                        {{ $dto->getSendTime() }}
-                    </p>
+                <div class="card m-b-20 card-inverse text-black" style="background-color: #f0f1f4; border-color: white;">
+                    <div class="card-block">
+                        <div class="row">
+                            <div class="col-md-4">STATUS</div>
+                            <div class="col-md-8">{{ $dto->getProofStatus() }}</div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-4">SEND TIME</div>
+                            <div class="col-md-8">{{ $dto->getSendTime() }}</div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-<div class="row form-justify-container">
-    <div class="col-md-12">
-        <div class="box">
-            <div class="box-body">
-                <!-- Items Field -->
-                <ul class="proofs_list">
-                    @foreach($dto->getItems() as $item)
-                        <li class="proof__item">
-                            {!! $dto->buildItemTag($item) !!}
-                        </li>
-                    @endforeach
-                </ul>
             </div>
         </div>
     </div>
@@ -76,27 +41,62 @@
 @if($dto->isAbleForChangeStatus())
     {!! Form::open(['method'=>'PUT', 'url'=> route('challenge.proof.update', ['challenge' => $dto->getChallengeId(), 'proof' => $dto->getProofId()])]) !!}
     {{ Form::hidden('status', \App\Modules\Challenges\Enums\ProofStatusEnum::PENDING) }}
-        <div class="row form-justify-container">
+
+        <div class="row form-justify-container buttons-block">
             <div class="col-md-6">
-                <div class="box">
-                    <div class="box-body">
-                        <!-- Accept button -->
-                        <a class="change-proof-status-button btn btn-success" status="{{ \App\Modules\Challenges\Enums\ProofStatusEnum::ACCEPTED }}">Accept</a>
-                    </div>
+                <div class="button-items">
+                    <button class="change-proof-status-button btn-block btn btn-success" status="{{ \App\Modules\Challenges\Enums\ProofStatusEnum::ACCEPTED }}">ACCEPT</button>
                 </div>
             </div>
             <div class="col-md-6">
-                <div class="box">
-                    <div class="box-body">
-                        <!-- Reject button -->
-                        <a class="change-proof-status-button btn btn-danger" status="{{ \App\Modules\Challenges\Enums\ProofStatusEnum::REJECTED }}">Reject</a>
-                    </div>
+                <div class="button-items">
+                    <button class="change-proof-status-button btn-block btn btn-danger" status="{{ \App\Modules\Challenges\Enums\ProofStatusEnum::REJECTED }}">REJECT</button>
                 </div>
             </div>
         </div>
     <!-- Submit Field -->
-    {!! Form::submit('Save', ['class' => 'hidden-submit']) !!}
+    <button hidden id="change-status" data-toggle="tooltip" data-placement="top" title="Change-status"
+            type="submit" class="dropdown-item"
+            onclick="return confirm('Are you sure you want changes proof status?');">
+    </button>
     {!! Form::close() !!}
+
 @endif
+
+<div class="row form-justify-container">
+    <div class="col-md-12">
+        <div class="card m-b-20 card-inverse text-white">
+            <div class="card-block">
+                <h4 class="mt-0 header-title proof__title">Proof items</h4>
+            </div>
+
+            <div class="card-block">
+                @foreach($dto->getItems() as $item)
+                    @if($dto->isVideoProof())
+                        <div class="media m-b-30 img-responsive proof__container video_proof">
+                            <video controls width="70%">
+                                <source src="{{ $item }}" type="video/mp4">
+                            </video>
+                            <div class="video-format-no-supported">
+                                <div class="card">
+                                    <div class="card-block">
+                                        <p> Sorry, your browser does not support this video format.</p>
+                                        <p>Use another browser or download this video <a href="{{ $item }}" download>Download</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @else
+                        <div class="media m-b-30 img-responsive proof__container">
+                            <img class="mr-3 img-thumbnail proof__item" src="{{ $item }}">
+                        </div>
+                    @endif
+                @endforeach
+            </div>
+        </div>
+    </div>
+</div>
+
+
 
 
